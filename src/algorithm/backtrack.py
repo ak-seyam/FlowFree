0e85ec2763d47a5de42a52849a081c9e8e1f04c8
A-Siam <abdullah.khaled.kamal0@gmail.com>
2020-12-19 14:56:38 +0200

    feat: create basic skeleton

---
+++ src/algorithm/backtrack.py
@@ -0,0 +1,43 @@
+from ..model.state import state
+
+
+def backtrack(csp,
+              select_unassigned_variable,
+              order_domain_values,
+              assignment_complete,
+              get_inferences,
+              is_consistant):
+    return _backtrack([], csp, select_unassigned_variable, order_domain_values, assignment_complete, get_inferences, is_consistant)
+
+
+def _backtrack(assignments: list,
+               csp,
+               select_unassigned_variable,
+               order_domain_values,
+               assignment_complete,
+               get_inferences,
+               is_consistant):
+    if assignment_complete(assignments):
+        return assignments
+    var = select_unassigned_variable(csp)
+    for value in order_domain_values():
+        if is_consistant({var: value}, assignments, csp):
+            assignments.append(value)
+            inferences = get_inferences()  # TODO rewrite this after you study consistency
+            # TODO check this snippet again
+            if inferences != state.failure:
+                assignments.extend(inferences)
+                res = _backtrack(
+                    assignments,
+                    csp,
+                    select_unassigned_variable,
+                    order_domain_values,
+                    assignment_complete,
+                    get_inferences,
+                    is_consistant
+                )
+            if res != state.failure:
+                return res
+        # removing failure from assignments doesn't make sense imp
+        assignments.remove({var: value})
+    return state.failure
---
+++ src/model/state.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class state(Enum):
+    success = 'Success'
+    failure = 'failure'
+    
