378327cb97eff57cecf725f262d7ca24d86a0299
abdo ragab hashem <ar1813@fayoum.edu.eg>
2021-01-07 15:59:01 +0200

    clean comment in formatter

--- src/utils/formater.py
+++ src/utils/formater.py
@@ -1,23 +1,9 @@
-from pprint import pprint
 def formatter(inp:dict, width, height, init=None):
-    pprint(inp)
     res = [[init for i in range(width)] for j in range(height)] 
     for key in inp :
-        print('key 1', key[1], 'key 0', key[0])
         res[key[1]][key[0]] = inp[key]
 
     for i in range(len(res)):
         for j in range(len(res[0])):
             print(res[i][j],end="")
         print()
-
-# testing example
-# example = {
-# 	(0,0) : 'a',
-# 	(0,1) : 'b',
-# 	(0,2) : 'c',
-# 	(1,0) : 'd',
-# 	(1,1) : 'e',
-# 	(1,2) : 'f'
-# }
-# formatter(example,2,3)
