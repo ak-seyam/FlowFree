4e83fb660612742f1491d8b2c972d5090ef21ceb
abdo ragab hashem <ar1813@fayoum.edu.eg>
2021-01-07 20:23:56 +0200

    solve for multible pathes with time report

--- src/main_smart.py
+++ src/main_smart.py
@@ -5,27 +5,45 @@ from algorithm import smart
 from utils.paths.initial_state import get_initial_state
 from utils.formater import formatter, formatter
 from random import seed
+import time
 seed(0)
 
 paths = [
-    "../input/input1214.txt"
+"../input/input55.txt",
+"../input/input77.txt",
+"../input/input88.txt",
+"../input/input991.txt",
+"../input/input10101.txt",
+"../input/input10102.txt",
+# "../input/input1212.txt",
+# "../input/input1214.txt",
+# "../input/input1414.txt",
 ]
+for path in paths:
+    inp = read_inputfile(path)
+    # print(inp)
 
-inp = read_inputfile(paths[0])
-# print(inp)
+    initial_state = get_initial_state(inp)
+    # print(initial_state[1])
 
-initial_state = get_initial_state(inp)
-# print(initial_state[1])
+    def debug_print(assignments,domain,var,val):
+        print(f'{var} ,{val}')
+        print(f'domain {domain[var]}')
+        formatter(assignments,len(inp[0]), len(inp),init="_")
 
-res = backtrack(
-    initial_state,
-    initial_state[1],
-    inp,
-    smart.order_domain_values,
-    dum.assignment_complete,
-    dum.inference,
-    dum.is_consistant,
+    start = time.time()
+    res = backtrack(
+        initial_state,
+        initial_state[1],
+        inp,
+        smart.order_domain_values,
+        dum.assignment_complete,
+        dum.inference,
+        dum.is_consistant,
+        lambda *args: 1,
     lambda *args: 1, 
-    smart.get_var
-)
-formatter(res, len(inp[0]), len(inp))
+        lambda *args: 1,
+        smart.get_var
+    )
+    print(f'map {path} solution time = {time.time()-start} sec')
+    formatter(res, len(inp[0]), len(inp))
