e55155fdf313b8e74c004d7285a5f9b8875f3341
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-01-15 15:34:01 +0200

    feat: add dumb config

--- src/algorithm/dummy.py
+++ src/algorithm/dummy.py
@@ -4,6 +4,9 @@ from model.case import case
 from utils.paths.path_state import is_good_combination, terminal_with_two_same_color_exist, is_neighbors_terminal_have_vaild_path
 from random import random, shuffle
 
+config = {
+    "totally_random" : False
+}
 
 def assignment_complete(assignments, inp):
     """
@@ -40,11 +43,14 @@ def select_unassigned_variable(variables_domain, assignments: dict, inp):
     for i in range(len(inp)):
         for j in range(len(inp[0])):
             if assignments.get((i, j)) == None:
-                # _tmp.append((i, j))
-                return (i,j)
-    # rand_index = int(random()*len(_tmp) // 1)
+                if config.get("totally_random"):
+                    _tmp.append((i, j))
+                else :
+                    return (i,j)
 
-    # return _tmp[rand_index]
+    rand_index = int(random()*len(_tmp) // 1)
+
+    return _tmp[rand_index]
 
 
 def order_domain_values(initial_state, assignments, inp, var, variables_domain, *arg):
